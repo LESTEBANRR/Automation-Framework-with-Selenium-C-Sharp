9d70670d550bba3dca635df6694821009e75736b
LESTEBANRR <l_esteban_1998@hotmail.com>
2020-07-01 15:43:16 -0500

    Clase 42 Configuration Develpment part a

---
+++ EAEmployeeTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EAEmployeeTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("EAEmployeeTest")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7f0e58d3-bd6d-4220-a909-3fcfb7617d71")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EAEmployeeTest/UnitTest1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using EAEmployeeTest.Pages;
+using EAAutoFramework.Base;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Chrome;
+using EAAutoFramework.Helpers;
+using EAAutoFramework.Config;
+
+namespace EAEmployeeTest
+{
+    [TestClass]
+    public class UnitTest1 : Base
+    {
+
+
+        string url = ConfigReader.InitializeTest();
+
+
+        public void OpenBrowser(BrowserType browserType = BrowserType.FireFox)
+        {
+            switch (browserType)
+            {
+                case BrowserType.InternetExplorer:
+                    DriverContext.Driver = new InternetExplorerDriver();
+                    DriverContext.Browser = new Browser(DriverContext.Driver);
+                    break;
+                case BrowserType.FireFox:
+                    DriverContext.Driver = new FirefoxDriver();
+                    DriverContext.Browser = new Browser(DriverContext.Driver);
+                    break;
+                case BrowserType.Chrome:
+                    DriverContext.Driver = new ChromeDriver();
+                    DriverContext.Browser = new Browser(DriverContext.Driver);
+                    break;
+            }
+
+        }
+
+
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //DriverContext.Driver = new FirefoxDriver();
+            //DriverContext.Driver.Navigate().GoToUrl(url);
+
+
+            string fileName = Environment.CurrentDirectory.ToString() + "\\Data\\Login.xlsx";
+
+            ExcellHelpers.PopulateInCollection(fileName);
+
+            LogHelpers.CreateLogFile();
+
+            OpenBrowser(BrowserType.Chrome);
+            LogHelpers.Write("Opened the browser !!!");
+
+            DriverContext.Browser.GoToUrl(url);
+            LogHelpers.Write("Navigated to the page !!!");
+
+            //LoginPage
+            CurrentPage = GetInstance<LoginPage>();
+            CurrentPage.As<LoginPage>().ClickLoginLink();
+            CurrentPage.As<LoginPage>().Login(ExcellHelpers.ReadData(1, "UserName"), ExcellHelpers.ReadData(1, "Password"));
+            //EmployeePage
+            CurrentPage = CurrentPage.As<LoginPage>().ClickEmployeeList();
+            CurrentPage.As<EmployeePage>().ClickCreateNew();
+
+
+        }
+
+        [TestMethod]
+        public void TableOperation()
+        {
+            string fileName = Environment.CurrentDirectory.ToString() + "\\Data\\Login.xlsx";
+
+            ExcellHelpers.PopulateInCollection(fileName);
+
+            LogHelpers.CreateLogFile();
+
+            OpenBrowser(BrowserType.FireFox);
+            LogHelpers.Write("Opened the browser !!!");
+
+            DriverContext.Browser.GoToUrl(url);
+            LogHelpers.Write("Navigated to the page !!!");
+
+            CurrentPage = GetInstance<LoginPage>();
+            CurrentPage.As<LoginPage>().ClickLoginLink();
+            CurrentPage.As<LoginPage>().Login(ExcellHelpers.ReadData(1, "UserName"), ExcellHelpers.ReadData(1, "Password"));
+            //EmployeePage
+            CurrentPage = CurrentPage.As<LoginPage>().ClickEmployeeList();
+
+            var table = CurrentPage.As<EmployeePage>().GetEmployeeList();
+
+            HtmlTableHelper.ReadTable(table);
+            HtmlTableHelper.PerformActionOnCell("5", "Name", "Ramesh", "Benefits");
+
+
+
+        }
+
+
+    }
+}
