2dfd5b4af9fe5c29fa3317c31b1bf6a0d5ad30d5
LESTEBANRR <l_esteban_1998@hotmail.com>
2020-07-01 17:29:33 -0500

    Clase 47 Web Element Extension 2

--- EATestProject/Extensions/WebElementExtensions.cs
+++ EATestProject/Extensions/WebElementExtensions.cs
@@ -1,15 +1,37 @@
-﻿using OpenQA.Selenium;
+﻿using EAAutoFramework.Base;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace EAAutoFramework.Extensions
 {
     public static class WebElementExtensions
     {
+        public static string GetSelectedDropDown(this IWebElement element)
+        {
+            SelectElement ddl = new SelectElement(element);
+            return ddl.AllSelectedOptions.First().ToString();
+        }
+
+        public static IList<IWebElement> GetSelectedListOptions(this IWebElement element)
+        {
+            SelectElement ddl = new SelectElement(element);
+            return ddl.AllSelectedOptions;
+        }
+
         public static void SelectDropDownList(this IWebElement element,string value)
         {
             SelectElement ddl = new SelectElement(element);
             ddl.SelectByText(value);
+        }
+
+        public static void Hover(this IWebElement element)
+        {
+            Actions actions = new Actions(DriverContext.Driver);
+            actions.MoveToElement(element).Perform();
         }
         public static void AssertElementPresent(this IWebElement element)
         {
